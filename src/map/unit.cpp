73d38b304e7cb7c7f9e9ba2f323076acdfb3e0f1
Seravy <SeravySensei@gmail.com>
2019-12-03 15:21:31 +0100

    SeraRO

--- src/map/unit.cpp
+++ src/map/unit.cpp
@@ -93,8 +93,8 @@ int unit_walktoxy_sub(struct block_list *bl)
 	if( !path_search(&wpd,bl->m,bl->x,bl->y,ud->to_x,ud->to_y,ud->state.walk_easy,CELL_CHKNOPASS) )
 		return 0;
 
-	if (!path_search_long(NULL, bl->m, bl->x, bl->y, ud->to_x, ud->to_y, CELL_CHKNOPASS) // Check if there is an obstacle between
-		&& wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
+	if (!(path_search_long(NULL, bl->m, bl->x, bl->y, ud->to_x, ud->to_y, CELL_CHKNOPASS, MAX_WALKPATH)) // Check if there is an obstacle between
+		&& (wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT)	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
 		&& (bl->type != BL_NPC)) // If type is a NPC, please disregard.
 	{
 		if (bl->type == BL_MOB) return 0; // prevent monsters jumping through walls, client can't display
@@ -677,8 +677,9 @@ int unit_walktoxy( struct block_list *bl, short x, short y, unsigned char flag)
 	if (!path_search(&wpd, bl->m, bl->x, bl->y, x, y, flag&1, CELL_CHKNOPASS)) // Count walk path cells
 		return 0;
 
-	if (!path_search_long(NULL, bl->m, bl->x, bl->y, x, y, CELL_CHKNOPASS) // Check if there is an obstacle between
-		&& wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
+	if (!(path_search_long(NULL, bl->m, bl->x, bl->y, x, y, CELL_CHKNOPASS, MAX_WALKPATH)))
+		if // Check if there is an obstacle between
+		((wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT)	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
 		&& (bl->type != BL_NPC)) // If type is a NPC, please disregard.
 	{
 		if (bl->type == BL_MOB) return 0; // prevent monsters jumping through walls, client can't display
@@ -2479,8 +2480,8 @@ bool unit_can_reach_bl(struct block_list *bl,struct block_list *tbl, int range, 
 	if (!path_search(&wpd,bl->m,bl->x,bl->y,tbl->x-dx,tbl->y-dy,easy,CELL_CHKNOREACH))
 		return false;
 
-	if (!path_search_long(NULL, bl->m, bl->x, bl->y, tbl->x - dx, tbl->y - dy, CELL_CHKNOPASS) // Check if there is an obstacle between
-		&& wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
+	if (!(path_search_long(NULL, bl->m, bl->x, bl->y, tbl->x - dx, tbl->y - dy, CELL_CHKNOPASS, MAX_WALKPATH)) // Check if there is an obstacle between
+		&& (wpd.path_len > CLIENT_COMPLEX_PATH_LIMIT)	// Official number of walkable cells is 14 if and only if there is an obstacle between. [malufett]
 		&& (bl->type != BL_NPC)) // If type is a NPC, please disregard.
 	{
 		if (bl->type == BL_MOB) return false;
