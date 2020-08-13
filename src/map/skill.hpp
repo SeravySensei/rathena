d85b60619c26dc1ccb2d427f6c84094cf864fc5a
Seravy <SeravySensei@gmail.com>
2020-08-13 14:49:43 +0200

    Revert "Job Improvement Project - Warlock (#4897)"
    This reverts commit 36195d157d6eef99056926190468bf2e25f4dbed.

--- src/map/skill.hpp
+++ src/map/skill.hpp
@@ -1965,7 +1965,6 @@ enum e_skill {
 	NV_BREAKTHROUGH,
 	NV_HELPANGEL,
 	NV_TRANSCENDENCE,
-	WL_READING_SB_READING,
 
 	HLIF_HEAL = 8001,
 	HLIF_AVOID,
@@ -2262,7 +2261,7 @@ void skill_usave_trigger(struct map_session_data *sd);
 /**
  * Warlock
  **/
-enum e_wl_spheres {
+enum wl_spheres {
 	WLS_FIRE = 0x44,
 	WLS_WIND,
 	WLS_WATER,
